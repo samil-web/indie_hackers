ecf48496af71fc987409b16b42dd337acd04e5f4
Shamil Aliyev <samil.alieff@gmail.com>
2023-10-31 06:51:09 +0100

    changed staticroute to slug!

--- app/reviews/[slug]/page.jsx
+++ app/reviews/[slug]/page.jsx
@@ -2,29 +2,32 @@
 import Heading from "@/components/Heading"
 import ShareLinkButton from "@/components/ShareLinkButton"
 import { getReview, getStatic } from "@/lib/reviews"
+import { notFound } from 'next/navigation';
 
 export async function generateStaticParams(){
     
     const statics = await getStatic()
-    return statics.map((staticroute) => ({staticroute}))
+    return statics.map((slug) => ({slug}))
 }
 
 export async function generateMetadata({ params:{slug} }){
-    const {title} = await getReview(slug)
-    return({title})
+    const review = await getReview(slug)
+    return{
+        title: review.title
+    }
 }
 export default async function ReviewPage({params:{slug}}){
-    const {title,date,image,body} = await getReview(slug)
+    const review = await getReview(slug)
     return(
         <>
-        <Heading>{title}</Heading>
+        <Heading>{review.title}</Heading>
         <div className="flex gap-3 items-baseline">
-        <p className="italic mb-2">{date}</p>
+        <p className="italic mb-2">{review.date}</p>
         <ShareLinkButton/>
         </div>
-        <img src={image} alt="" width={640} height={360}
+        <img src={review.image} alt="" width={640} height={360}
         className="mb-2 rounded"></img>
-        <article dangerouslySetInnerHTML={{__html: body}}
+        <article dangerouslySetInnerHTML={{__html: review.body}}
          className="prose prose-slate max-w-screen-sm"
         >
         </article>
